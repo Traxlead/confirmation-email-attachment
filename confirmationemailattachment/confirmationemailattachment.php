bf3e899f3282a94b0de20a807e26086852e9c4fc
albangonzalez <alban.gonzalez@mail.com>
2017-05-24 19:32:38 +0200

    Initialisation dépôt module PrestaShop confirmation-email-attachment

---
+++ confirmationemailattachment/confirmationemailattachment.php
@@ -0,0 +1,230 @@
+<?php
+/**
+* 2007-2017 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2017 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class ConfirmationEmailAttachment extends Module
+{
+    protected $config_form = false;
+
+    protected $target_directory = _PS_ROOT_DIR_ . '/upload/';
+
+    public function __construct()
+    {
+        $this->name = 'confirmationemailattachment';
+        $this->tab = 'emailing';
+        $this->version = '0.1.0';
+        $this->author = 'Traxlead';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Confirmation Email Attachment');
+        $this->description = $this->l('Allows administrator to attach a document in the Order Confirmation Email.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('CEA_ENABLED');
+        Configuration::deleteByName('CEA_ATTACHMENT_FILE');
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitConfirmationEmailAttachmentModule')) == true) {
+            $this->postProcess();
+        }
+
+        $attachment_path = str_replace(_PS_ROOT_DIR_, '', Configuration::get('CEA_ATTACHMENT_FILE'));
+        $attachment_name = str_replace($this->target_directory, '', Configuration::get('CEA_ATTACHMENT_FILE'));
+
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+        $this->context->smarty->assign('attachment_path', $attachment_path);
+        $this->context->smarty->assign('attachment_name', $attachment_name);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitConfirmationEmailAttachmentModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Enabled'),
+                        'name' => 'CEA_ENABLED',
+                        'is_bool' => true,
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Yes')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('No')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 8,
+                        'type' => 'file',
+                        'desc' => $this->l('Chose a file to attach with your order confirmation email'),
+                        'name' => 'CEA_ATTACHMENT_FILE',
+                        'label' => $this->l('Attachment'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        // return array(
+        //     'ORDEREMAILATTACHMENT_LIVE_MODE' => Configuration::get('ORDEREMAILATTACHMENT_LIVE_MODE', true),
+        //     'ORDEREMAILATTACHMENT_ACCOUNT_EMAIL' => Configuration::get('ORDEREMAILATTACHMENT_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+        //     'ORDEREMAILATTACHMENT_ACCOUNT_PASSWORD' => Configuration::get('ORDEREMAILATTACHMENT_ACCOUNT_PASSWORD', null),
+        // );
+
+        return array(
+            'CEA_ENABLED'         => Configuration::get('CEA_ENABLED'),
+            'CEA_ATTACHMENT_FILE' => Configuration::get('CEA_ATTACHMENT_FILE')
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        // $form_values = $this->getConfigFormValues();
+
+        $target_file =  $this->target_directory . $_FILES['CEA_ATTACHMENT_FILE']['name'];
+
+        move_uploaded_file($_FILES['CEA_ATTACHMENT_FILE']['tmp_name'], $target_file);
+
+        if(!empty($_FILES['CEA_ATTACHMENT_FILE']['tmp_name']))
+            Configuration::updateValue('CEA_ATTACHMENT_FILE', $target_file);
+        
+        Configuration::updateValue('CEA_ENABLED', Tools::getValue('CEA_ENABLED'));
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+}
---
+++ confirmationemailattachment/translations/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_41fbf715c9436819ce37b7aaae488cc0'] = 'Confirmation de commande (Pièce jointe d\'email)';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_4a5982ee9c48fb650f32492e5ab562c3'] = 'Permet d\'inclure une pièce jointe supplémentaire dans l\'e-mail de confirmation de commande.';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_876f23178c29dc2552c0b48bf23cd9bd'] = 'Êtes-vous sûr de vouloir désinstaller ce module ?';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_f4f70727dc34561dfde1a3c529b6205c'] = 'Paramètres';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activé';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_93cba07454f06a4a960172bbd6e2a435'] = 'Oui';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Non';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_a2f1137662a538e9b4c14124052b0de9'] = 'Choisissez une pièce jointe accompagnant l\\\'e-mail de confirmation de commande';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_e9cb217697088a98b1937d111d936281'] = 'Pièce jointe';
+$_MODULE['<{confirmationemailattachment}prestashop>confirmationemailattachment_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{confirmationemailattachment}prestashop>configure_a82be0f551b8708bc08eb33cd9ded0cf'] = 'Information';
+$_MODULE['<{confirmationemailattachment}prestashop>configure_a5251d0b9619ffa0ee6f9e5e8fa81a7a'] = 'Veuillez choisir un document qui sera inclus en tant que pièce jointe dans l\\\'e-mail de confirmation de commande (format de document recommandé : fichier PDF).';
+$_MODULE['<{confirmationemailattachment}prestashop>configure_f561a2f98ef861de6d77a068e42f9007'] = 'Dernier document mis en ligne : ';
